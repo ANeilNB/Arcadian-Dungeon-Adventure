10120b9d8b64b13ff99412183a542a22dd23b3b6
Alexander Neil <Alexander.Neil@netbuilder.com>
2015-07-24 23:15:33 +0100

    Variables added. - AN

--- src/game/Character.java
+++ src/game/Character.java
@@ -1,5 +1,16 @@
 package game;
 
 public class Character {
-
+	
+	private String name;
+	private int strength;
+	private int dexterity;
+	private int intelligence;
+	private int maxHitPoints;
+	private int currentHitPoints;
+	
+	private int locationX;
+	private int locationY;
+	
+	private ArrayList<Item> inventory;
 }
