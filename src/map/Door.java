fdcbd06c096359890cac759e35db48fe220bc38f
MattWatsonNB <Matthew.Watson@netbuilder.com>
2015-07-25 08:15:19 +0100

    Door

--- src/map/Door.java
+++ src/map/Door.java
@@ -5,6 +5,26 @@ public class Door extends Openable {
 	private Boolean isUnlocked;
 	private Boolean isTrap;
 	
+	public Door() {
+		isUnlocked = false;
+		isTrap = false;
+	}
+
+	public Boolean getIsUnlocked() {
+		return isUnlocked;
+	}
+
+	public void setIsUnlocked(Boolean isUnlocked) {
+		this.isUnlocked = isUnlocked;
+	}
+
+	public Boolean getIsTrap() {
+		return isTrap;
+	}
+
+	public void setIsTrap(Boolean isTrap) {
+		this.isTrap = isTrap;
+	}
 	
 	
 }
