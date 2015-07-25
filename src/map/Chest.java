a519dd1b06b18b32a37e4c991e5269341e24f48c
MattWatsonNB <Matthew.Watson@netbuilder.com>
2015-07-25 06:21:33 +0100

    Chest Completed....maybe

--- src/map/Chest.java
+++ src/map/Chest.java
@@ -1,15 +1,59 @@
 package map;
 
+import game.Item;
+
 public class Chest extends Tile {
 	private Boolean isUnlocked;
 	private Boolean isTrap;
 	
+	private Item item;
 	
+	public Chest(char chestChar) {
+		this.isUnlocked = false;
+		this.isTrap = false;
+		
+		switch(chestChar) {
+			case 'r' : item = new Item("rope");
+						break;
+			case 'g' : item = new Item("glovesOfStr");
+						break;
+			case 'i' : item = new Item("crownOfInt");
+						break;
+			case 'y' : item = new Item("bootsOfDex");
+						break;
+			case 'l' : item = new Item("ringOfLuck");
+						break;
+		}
+	}
+
+	public Boolean getIsUnlocked() {
+		return isUnlocked;
+	}
+
+	public void setIsUnlocked(Boolean isUnlocked) {
+		this.isUnlocked = isUnlocked;
+	}
+
+	public Boolean getIsTrap() {
+		return isTrap;
+	}
+
+	public void setIsTrap(Boolean isTrap) {
+		this.isTrap = isTrap;
+	}
+
+	public Item getItem() {
+		return item;
+	}
+
+	public void setItem(Item item) {
+		this.item = item;
+	}
+
 	@Override
 	public void writeDescription() {
 		// TODO Auto-generated method stub
 		
 	}
-	
-	
+		
 }
