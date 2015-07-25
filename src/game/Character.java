7a0e10f583a0755c32ce911eb6e9e94071dc5326
MattWatsonNB <Matthew.Watson@netbuilder.com>
2015-07-25 01:36:36 +0100

    Character and Item(Partially Done)

--- src/game/Character.java
+++ src/game/Character.java
@@ -1,5 +1,7 @@
 package game;
 
+import java.util.ArrayList;
+
 public class Character {
 	
 	private String name;
@@ -13,4 +15,79 @@ public class Character {
 	private int locationY;
 	
 	private ArrayList<Item> inventory;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getDexterity() {
+		return dexterity;
+	}
+
+	public void setDexterity(int dexterity) {
+		this.dexterity = dexterity;
+	}
+
+	public int getIntelligence() {
+		return intelligence;
+	}
+
+	public void setIntelligence(int intelligence) {
+		this.intelligence = intelligence;
+	}
+
+	public int getMaxHitPoints() {
+		return maxHitPoints;
+	}
+
+	public void setMaxHitPoints(int maxHitPoints) {
+		this.maxHitPoints = maxHitPoints;
+	}
+
+	public int getCurrentHitPoints() {
+		return currentHitPoints;
+	}
+
+	public void setCurrentHitPoints(int currentHitPoints) {
+		this.currentHitPoints = currentHitPoints;
+	}
+
+	public int getLocationX() {
+		return locationX;
+	}
+
+	public void setLocationX(int locationX) {
+		this.locationX = locationX;
+	}
+
+	public int getLocationY() {
+		return locationY;
+	}
+
+	public void setLocationY(int locationY) {
+		this.locationY = locationY;
+	}
+
+	public ArrayList<Item> getInventory() {
+		return inventory;
+	}
+
+	public void setInventory(ArrayList<Item> inventory) {
+		this.inventory = inventory;
+	}
+	
+	
+	
 }
--- src/map/Openable.java
+++ src/map/Openable.java
@@ -2,5 +2,6 @@ package map;
 
 public class Openable extends Tile {
 
+	private Boolean isOpenable;
 	
 }
