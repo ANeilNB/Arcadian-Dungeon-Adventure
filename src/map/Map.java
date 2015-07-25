e81ee7d4f60d8a3479f63b7d7a28331c12461a0e
Alexander Neil <Alexander.Neil@netbuilder.com>
2015-07-25 02:42:18 +0100

    Added checking of validity of map tiles. - AN

--- src/map/Map.java
+++ src/map/Map.java
@@ -1,14 +1,29 @@
 package map;
 
+import game.MapGenerator;
+
 public class Map {
 
 	
 	private char[][] map;
 	private Tile[][] tiles;
 	
+	public Map(String filepath){
+		map = MapGenerator.createMap(filepath);
+		
+		/*
+		for(int i=0;i<map.length;i++){
+			for(int j=0;j<map[i].length;j++){
+				System.out.print(map[i][j]);
+			}
+			System.out.print("\n");
+		}
+		*/
+	}
+	
 	public Tile getTile(int xVal, int yVal){
 		
-		if(xVal < map.length && yVal < map[xVal].length) return tiles[xVal][yVal];
+		if(xVal < map.length && yVal < map[xVal].length) return tiles[yVal][xVal];
 		else{
 			System.err.println("Map Error");
 			return null;
@@ -16,16 +31,34 @@ public class Map {
 	}
 	
 	public char getType(int xVal, int yVal){
-		if(xVal < map.length && yVal < map[xVal].length) return map[xVal][yVal];
+		if(xVal < map.length && yVal < map[xVal].length) return map[yVal][xVal];
 		else return '0';
 	}
 	
 	public void enterTile(int xVal, int yVal){
 		if(getTile(xVal, yVal) == null){
-			char type = getType(xVal, yVal);
+			char type = getType(yVal, xVal);
 			switch(type){
 				//cases for each character making relevant class
 			}
+		}
+	}
+	
+	public boolean isInteractable(int xVal, int yVal){
+		try{
+			char tile = getType(xVal,yVal);
+			
+			switch(tile){
+			case 'e':
+				return false;	
+			case 'o':
+				return false;
+			default:
+				return true;
+			}
+		}
+		catch(ArrayIndexOutOfBoundsException aobe){
+			return false;
 		}
 	}
 }
